5ee9fbbaea4dad9395a6fa96a93fe7fa86d3c663
Weegoos <batyr.ashim05@mail.ru>
2024-10-17 21:23:47 +0500

    Tennis project: Unnecessary components have been removed

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,9 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>Batyr</div>
 </template>
 
-<script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+<script>
+export default {};
 </script>
+
+<style></style>
