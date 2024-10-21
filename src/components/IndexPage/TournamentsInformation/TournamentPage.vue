f8e1e97aacfff5b97ab403a1d19ccb783f4674c3
Weegoos <batyr.ashim05@mail.ru>
2024-10-21 17:27:50 +0500

    Tennis Project: News have been updated

--- src/components/IndexPage/TournamentsInformation/TournamentPage.vue
+++ src/components/IndexPage/TournamentsInformation/TournamentPage.vue
@@ -22,7 +22,26 @@
       <q-tab-panels v-model="tab" animated>
         <q-tab-panel name="all">
           <div class="text-h6"></div>
-          Все новости будут отображаться здесь
+          <div class="row q-gutter-md">
+            <div class="col">
+              <q-img
+                src="https://images.unsplash.com/photo-1530915534664-4ac6423816b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                :ratio="16 / 9"
+                spinner-color="primary"
+                spinner-size="82px"
+              >
+                <caption class="absolute-bottom-left text-white q-pa-sm">
+                  <span class="bg-primary q-pa-sm date relative-position"
+                    >14 июля</span
+                  >
+                  <div class="text-bold text-h6">
+                    SILK ROAD OPEN 12-13-14 СЕНТЯБРЯ 2024 года
+                  </div>
+                </caption>
+              </q-img>
+            </div>
+            <div class="col"></div>
+          </div>
         </q-tab-panel>
 
         <q-tab-panel name="interview">
@@ -45,4 +64,10 @@ import { ref } from "vue";
 const tab = ref("all");
 </script>
 
-<style></style>
+<style scoped>
+.date {
+  border-top-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  left: -43%;
+}
+</style>
