17e54272415dd706fb987e312d944a1e4b624694
Weegoos <batyr.ashim05@mail.ru>
2024-10-22 17:37:27 +0500

    Tennis Project: Footer Button has been added

--- src/pages/FooterPage.vue
+++ src/pages/FooterPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="full-width text-white q-pa-md">
-    <div class="row text-center">
+    <div class="row text-center" style="justify-content: center">
       <section
         class="text-center"
         v-for="(button, index) in footerButton"
@@ -20,7 +20,19 @@ const footerButton = ref([
     name: "Новости",
   },
   {
-    name: "Новости",
+    name: "Контакты",
+  },
+  {
+    name: "О нас",
+  },
+  {
+    name: "Пользовательское соглашение",
+  },
+  {
+    name: "Политика конфиденциальности",
+  },
+  {
+    name: "Регламент турнира",
   },
 ]);
 </script>
