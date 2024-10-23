726137f945ebe544376798d898d1ea604a5437ec
Weegoos <batyr.ashim05@mail.ru>
2024-10-23 08:31:57 +0500

    Tennis Project: Stats has been added to the Achievements Page

--- src/pages/AchievementsPage.vue
+++ src/pages/AchievementsPage.vue
@@ -1,12 +1,40 @@
 <template>
   <div style="background-color: #24293e" class="text-white q-pa-xl">
     <p class="text-center text-h3 text-bold">Наши достижения</p>
-    <div class="text-center row">
-      <div class="col"></div>
+    <div class="row q-gutter-md">
+      <div
+        class="col q-pa-md"
+        v-for="(item, index) in achievements"
+        :key="index"
+        style="background-color: #1f2535"
+      >
+        <p>{{ item.stats }}</p>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const achievements = ref([
+  {
+    name: "Проведенные турниры",
+    stats: 48,
+  },
+  {
+    name: "Использованные мячи",
+    stats: 1510,
+  },
+  {
+    name: "Количество игроков",
+    stats: 222,
+  },
+  {
+    name: "Подписчиков",
+    stats: 333,
+  },
+]);
+</script>
 
 <style></style>
