57817377a2a17c7410cd779b2181b1878420d405
Weegoos <batyr.ashim05@mail.ru>
2024-10-23 08:40:32 +0500

    Tennis Project: About us page has been updated

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -8,11 +8,25 @@
       любительских турниров. Используя полученный опыт, мы постарались понять,
       что хочет получить игрок-любитель от участия в Турнире.
     </p>
+    <p class="text-body1">Нами был получен следующий результат:</p>
+    <ol
+      class="text-body1"
+      v-for="(item, index) in informationSection"
+      :key="index"
+    >
+      <li>{{ item.name }}</li>
+    </ol>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { ref } from "vue";
+
+const informationSection = ref([
+  {
+    name: "Лёгкая, быстрая регистрация онлайн.",
+  },
+]);
 </script>
 
 <style></style>
