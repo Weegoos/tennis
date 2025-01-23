8ad90672788a1ed3a0b890c29c412cb849f8454e
Weegoos <batyr.ashim05@mail.ru>
2025-01-23 23:03:22 +0500

    Tennis: The project has been updated

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -2,8 +2,32 @@
   <div>Профиль</div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { QSpinnerGears, useQuasar } from "quasar";
+import axios from "axios";
+import { useNotifyStore } from "src/stores/notify-store";
+
+const notifyStore = useNotifyStore();
+const $q = useQuasar();
+const getUserInformationById = async () => {
+  notifyStore.loading($q, "Подождите, данные грузятся...", QSpinnerGears);
+  try {
+    const response = await axios.get("http://localhost:8000/api/v1/user/1", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      withCredentials: true,
+    });
+
+    console.log(response.data);
+  } catch (error) {
+  } finally {
+    $q.loading.hide();
+  }
+};
+
+getUserInformationById();
 </script>
 
 <style></style>
