f45cf6b0754bb068f3791d977bf1d8494d523c01
Weegoos <batyr.ashim05@mail.ru>
2025-01-25 18:20:09 +0500

    Tennis: The project  has been updated

---
+++ src/components/Profile/GeneralInfo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>General Info</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>Профиль</div>
+  <div>
+    <section class="fixed-center">
+      <q-card class="my-card">
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </q-card-section>
+      </q-card>
+    </section>
+  </div>
 </template>
 
 <script setup>
 import { useNotifyStore } from "src/stores/notify-store";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useApiStore } from "src/stores/api-store";
 
 const notifyStore = useNotifyStore();
 const apiStore = useApiStore();
+
+const firstName = ref("");
+const secondName = ref("");
+const phone = ref("");
+const email = ref("");
 
 onMounted(async () => {
   await apiStore.getUserProfile();
