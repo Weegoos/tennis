03e84b56e097ec4891d247087107bbd0998f8da5
Weegoos <batyr.ashim05@mail.ru>
2025-01-26 00:21:22 +0500

    Tennis: FindPartnerPage.vue has been updated

--- src/pages/FindPartnerPage.vue
+++ src/pages/FindPartnerPage.vue
@@ -33,13 +33,27 @@
         </div>
       </div>
     </section>
-    <q-card class="card">
+    <q-card class="card" v-for="(items, index) in forms" :key="index">
+      <div class="row">
+        <div class="col">
+          <q-avatar
+            size="100px"
+            font-size="52px"
+            color="white"
+            text-color="grey"
+            icon="person"
+            fab
+          />
+          <span class="text-body1">{{ items.name }}</span>
+        </div>
+        <span color="green-4" class="col q-ma-md" align="right"
+          >NTRP {{ items.rating }}</span
+        >
+      </div>
       <q-card-section>
-        <div class="text-h6">Our Changing Planet</div>
-        <div class="text-subtitle2">by John Doe</div>
-      </q-card-section>
-      <q-card-section>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        <div class=""></div>
+        <div class="text-subtitle">{{ items.description }}</div>
+        <div class="text-subtitle">City: {{ items.city }}</div>
       </q-card-section>
     </q-card>
   </div>
@@ -52,13 +66,65 @@ const city = ref("Astana");
 const from = ref("From");
 const to = ref("To");
 const cities = ["Astana", "Almaty", "Karaganda"];
+
+const forms = ref([
+  {
+    name: "Batyr Ashim",
+    rating: 2.5,
+    city: "Astana",
+    description:
+      "An amateur tennis player who enjoys local tournaments and improving his serve.",
+  },
+  {
+    name: "Aruzhan Kairat",
+    rating: 3.5,
+    city: "Almaty",
+    description:
+      "A tennis coach specializing in beginner and intermediate players.",
+  },
+  {
+    name: "Murat Nurlan",
+    rating: 4,
+    city: "Shymkent",
+    description:
+      "A competitive tennis player known for his powerful backhand shots.",
+  },
+  {
+    name: "Aliya Talgat",
+    rating: 3.0,
+    city: "Karaganda",
+    description: "A recreational tennis enthusiast who loves doubles matches.",
+  },
+  {
+    name: "Yerlan Zhanibek",
+    rating: 3.0,
+    city: "Aktobe",
+    description:
+      "A professional tennis player focusing on international competitions.",
+  },
+  {
+    name: "Dana Beket",
+    rating: 3.5,
+    city: "Pavlodar",
+    description:
+      "An aspiring tennis player who trains daily to improve her forehand.",
+  },
+  {
+    name: "Rustem Omar",
+    rating: 4,
+    city: "Atyrau",
+    description:
+      "A seasoned tennis player known for his strategic gameplay and endurance.",
+  },
+]);
 </script>
 
 <style scoped>
 .card {
   margin-right: 200px;
   margin-left: 200px;
-  margin-top: 50px;
-  margin-bottom: 50px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+  border-radius: 10px;
 }
 </style>
