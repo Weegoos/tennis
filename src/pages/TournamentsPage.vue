82a701e00f46e2e42f98f5fa9ceaabde38add679
Weegoos <batyr.ashim05@mail.ru>
2025-02-04 13:06:32 +0500

    Diploma work: The tournaments has been added

--- src/pages/TournamentsPage.vue
+++ src/pages/TournamentsPage.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>Турниры</div>
+  <div>{{ tournaments }}</div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { Cookies, QSpinnerGears, useQuasar } from "quasar";
+import axios from "axios";
+import { useNotifyStore } from "src/stores/notify-store";
+import { ref } from "vue";
+
+// global variables
+const notifyStore = useNotifyStore();
+const $q = useQuasar();
+
+const tournaments = ref("");
+const getTournaments = async () => {
+  notifyStore.loading($q, "Подождите, данные загружаются...", QSpinnerGears);
+  try {
+    const response = await axios.get(
+      "http://localhost:8000/api/v1/tournament",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${Cookies.get("accessToken")}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    console.log(response.data);
+    tournaments.value = response.data;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    $q.loading.hide();
+  }
+};
+
+getTournaments();
 </script>
 
 <style></style>
