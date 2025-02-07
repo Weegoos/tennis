cca61895b2829970626ba1382d2d59cc3d08585e
Weegoos <batyr.ashim05@mail.ru>
2025-02-07 16:41:32 +0500

    Diploma work: The AuthPageComponent.test.js has been configured

--- test/vitest/__tests__/AuthPageComponent.test.js
+++ test/vitest/__tests__/AuthPageComponent.test.js
@@ -1,8 +1,23 @@
 import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
-import { describe, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+import AuthPage from "src/pages/AuthPage.vue";
+import { describe, it, vi } from "vitest";
+import { useRouter } from "vue-router";
 
 installQuasarPlugin();
 
+vi.mock("vue-router", () => ({
+  useRouter: vi.fn(),
+}));
+
 describe("Tests for AuthPageComponent", () => {
+  const pinia = createPinia();
+  const wrapper = mount(AuthPage, {
+    global: {
+      plugins: [pinia, Quasar],
+    },
+  });
   it("", () => {});
 });
