cd7e680099a62b36843d98899033960e911137ab
Weegoos <batyr.ashim05@mail.ru>
2025-02-11 15:49:58 +0500

    Tennis: The AdminAllCoaches has been configured

--- src/pages/Admin/AdminAllCoaches.vue
+++ src/pages/Admin/AdminAllCoaches.vue
@@ -1,9 +1,111 @@
 <template>
-  <div>Coaches</div>
+  <div class="q-pa-md" data-testid="rowsID" v-if="rows && rows.length">
+    <q-table
+      data-testid="userTable"
+      flat
+      bordered
+      :rows="rows"
+      :columns="columns"
+      row-key="id"
+      @row-click="viewDetailedInformation"
+    />
+    <UserDetailedInformation
+      :isOpenUserDetailedInformation="isOpenUserDetailedInformation"
+      :userInfo="userInfo"
+      @closeUserDetailedInformation="closeUserDetailedInformation"
+    />
+  </div>
+  <div v-else data-testid="noData">
+    <p>No data</p>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { Cookies, QSpinnerGears, useQuasar } from "quasar";
+import axios from "axios";
+import { useNotifyStore } from "src/stores/notify-store";
+import { getCurrentInstance, onMounted, ref } from "vue";
+
+// global variables
+const { proxy } = getCurrentInstance();
+const serverURL = proxy.$serverURL;
+const notifyStore = useNotifyStore();
+const $q = useQuasar();
+
+const columns = [
+  {
+    name: "id",
+    label: "№",
+    align: "left",
+    field: "id",
+    sortable: true,
+  },
+  {
+    name: "fullName",
+    label: "Full Name",
+    align: "left",
+    field: (coaches) => `${coaches.user.lastName} ${coaches.user.firstName}`,
+    sortable: true,
+  },
+  {
+    name: "city",
+    label: "City",
+    align: "left",
+    field: "city",
+    sortable: true,
+  },
+  {
+    name: "language",
+    label: "Language",
+    align: "left",
+    field: "language",
+    sortable: true,
+  },
+  {
+    name: "stadium",
+    label: "Stadium",
+    align: "left",
+    field: "stadium",
+    sortable: true,
+  },
+];
+
+const rows = ref([]);
+const getEnabledCoaches = async () => {
+  notifyStore.loading(
+    $q,
+    "Подождите данные о тренерах грузится...",
+    QSpinnerGears
+  );
+  try {
+    const response = await axios.get(`${serverURL}coach?enabled=false`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${Cookies.get("accessToken")}`,
+      },
+    });
+
+    console.log(response.data);
+    notifyStore.nofifySuccess($q, "Данные о тренере успешно загружены");
+    rows.value = response.data.map((user, index) => ({
+      ...user,
+      id: index + 1,
+    }));
+  } catch (error) {
+    console.log(error);
+    notifyStore.notifyError(
+      $q,
+      `Ошибка при получении данных о тренере: ${error}`
+    );
+  } finally {
+    $q.loading.hide();
+  }
+};
+
+onMounted(() => {
+  getEnabledCoaches();
+});
 </script>
 
 <style></style>
