1187487e9d0e9e34f6f6b1a091125f19387955a8
Weegoos <batyr.ashim05@mail.ru>
2025-02-17 17:47:04 +0500

    Diploma work: The project has been updated

--- src/components/Tournaments/ExploreTournament.vue
+++ src/components/Tournaments/ExploreTournament.vue
@@ -1,33 +1,35 @@
 <template>
   <div>
-    <p class="text-weight-regular text-h1 text-center q-mt-xl text-bold">{{ tournament.description }}</p>
-    <p class="text-weight-thin text-h5 text-center q-mt-xl text-bold">{{ tournament.location }} - {{ tournament.city }}</p>
+    <p class="text-weight-regular text-h1 text-center q-mt-xl text-bold">
+      {{ tournament.description }}
+    </p>
+    <p class="text-weight-thin text-h5 text-center q-mt-xl text-bold">
+      {{ tournament.location }} - {{ tournament.city }}
+    </p>
     <div class="q-pa-md">
-
-
-    <q-table
-      flat bordered
-      title="Additional information"
-      :rows="rows"
-      :columns="columns"
-      row-key="name"
-      :separator="separator"
-      hide-bottom
-    />
-  </div>
+      <q-table
+        flat
+        bordered
+        title="Additional information"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+        :separator="separator"
+        hide-bottom
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { Cookies, QSpinnerGears, useQuasar } from 'quasar';
-import axios from 'axios';
-import { useNotifyStore } from 'src/stores/notify-store';
-import { getCurrentInstance, onMounted, ref } from 'vue';
-
+import { Cookies, QSpinnerGears, useQuasar } from "quasar";
+import axios from "axios";
+import { useNotifyStore } from "src/stores/notify-store";
+import { getCurrentInstance, onMounted, ref } from "vue";
 
 // global variables
-const $q = useQuasar()
-const notifyStore = useNotifyStore()
+const $q = useQuasar();
+const notifyStore = useNotifyStore();
 const { proxy } = getCurrentInstance();
 const serverURL = proxy.$serverURL;
 
@@ -43,19 +45,61 @@ const columns = [
     name: "startTime",
     label: "Start Time",
     align: "left",
-    field: 'startTime',
+    field: "startTime",
     sortable: true,
   },
   {
     name: "startDate",
     label: "Start Date",
     align: "left",
-    field: 'startDate',
+    field: "startDate",
     sortable: true,
   },
-]
+  {
+    name: "endDate",
+    label: "End Date",
+    align: "left",
+    field: "endDate",
+    sortable: true,
+  },
+  {
+    name: "minLevel",
+    label: "Minimum level",
+    align: "left",
+    field: "minLevel",
+    sortable: true,
+  },
+  {
+    name: "maxLevel",
+    label: "Maximum level",
+    align: "left",
+    field: "maxLevel",
+    sortable: true,
+  },
+  {
+    name: "maxParticipants",
+    label: "Maximum participants",
+    align: "left",
+    field: "maxParticipants",
+    sortable: true,
+  },
+  {
+    name: "cost",
+    label: "Cost (тг)",
+    align: "left",
+    field: `cost`,
+    sortable: true,
+  },
+  {
+    name: "category",
+    label: "Category",
+    align: "left",
+    field: `category`,
+    sortable: true,
+  },
+];
 
-const rows = ref([])
+const rows = ref([]);
 
 const defineId = () => {
   const url = window.location.hash;
@@ -64,13 +108,13 @@ const defineId = () => {
   if (match) {
     const id = match[1];
     console.log(id);
-    getTournamentsByID(id)
+    getTournamentsByID(id);
   } else {
     console.log("Не найден id");
   }
 };
 
-const tournament = ref('')
+const tournament = ref("");
 const getTournamentsByID = async (id) => {
   notifyStore.loading($q, "Подождите, данные загружаются...", QSpinnerGears);
   try {
@@ -93,19 +137,24 @@ const getTournamentsByID = async (id) => {
         location: response.data.location,
         city: response.data.city,
         startDate: response.data.startDate,
-        endDate: response.data.endDate
-      }
+        endDate: response.data.endDate,
+        minLevel: response.data.minLevel,
+        maxLevel: response.data.maxLevel,
+        maxParticipants: response.data.maxParticipants,
+        cost: response.data.cost,
+        category: response.data.category,
+      },
     ];
   } catch (error) {
     console.error(error);
   } finally {
     $q.loading.hide();
   }
-}
+};
 
 onMounted(() => {
-  defineId()
-})
+  defineId();
+});
 </script>
 
 <style></style>
