6a9d81df6a3a20f5c8ac63f3eae483ce178839e0
Weegoos <batyr.ashim05@mail.ru>
2025-02-24 15:18:33 +0500

    Diploma work: The AdminAllCoaches.test.js has been created

---
+++ test/vitest/__tests__/AdminAllCoaches.test.js
@@ -0,0 +1,8 @@
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { describe, it } from "vitest";
+
+installQuasarPlugin();
+
+describe("Tests for AdminAllCoaches", () => {
+  it("", () => {});
+});
