d9cc21558f0ae3b822c376ce8b53c9b1f5edd9e2
Weegoos <batyr.ashim05@mail.ru>
2025-03-17 10:55:57 +0500

    The EditPartnerInformation.vue has been updated

--- src/components/FindPartner/EditPartnerInformation.vue
+++ src/components/FindPartner/EditPartnerInformation.vue
@@ -1,9 +1,20 @@
 <template>
   <q-dialog v-model="isOpenFindPartnetInformation" persistent>
     <q-card>
-      <q-card-section class="row q-gutter-sm">
-        <q-input v-model="firstName" type="text" label="First Name" />
-        <q-input v-model="lastName" type="text" label="Last Name" />
+      <q-card-section class="">
+        <section class="row q-gutter-sm">
+          <div class="col">
+            <q-input v-model="firstName" type="text" label="First Name" />
+            <q-input v-model="phone" type="number" label="Phone" />
+            <q-input v-model="city" type="text" label="City" />
+          </div>
+          <div class="col">
+            <q-input v-model="lastName" type="text" label="Last Name" />
+            <q-input v-model="rating" type="number" label="Rating" />
+            <q-input v-model="stadium" type="text" label="Stadium" />
+          </div>
+        </section>
+        <q-input v-model="description" type="text" label="Description" />
       </q-card-section>
       <q-card-actions align="right">
         <q-btn
@@ -55,13 +66,22 @@ const closeEditPartnerInformationPage = () => {
 
 const firstName = ref("");
 const lastName = ref("");
+const phone = ref("");
+const rating = ref("");
+const city = ref("");
+const stadium = ref("");
+const description = ref("");
 
 const partnerInfo = ref("");
 const updatePartnerInfo = async () => {
   const params = {};
-  if (firstName.value) params.firstName = city.firstName;
+  if (firstName.value) params.firstName = city.value.firstName;
   if (lastName.value) params.lastName = lastName.value;
-  console.log(props.partnerId);
+  if (phone.value) params.phone = phone.value;
+  if (rating.value) params.rating = rating.value;
+  if (city.value) params.city = city.value;
+  if (stadium.value) params.stadium = stadium.value;
+  if (description.value) params.description = description.value;
 
   await putMethod(
     serverURL,
