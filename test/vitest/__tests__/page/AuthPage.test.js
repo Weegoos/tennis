b008173cc2e3dcce1521ee44acc3e1dbed288512
Weegoos <batyr.ashim05@mail.ru>
2025-04-07 15:26:13 +0500

    Tennis: The AuthPage.test.js has been created

---
+++ test/vitest/__tests__/page/AuthPage.test.js
@@ -0,0 +1,7 @@
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { describe, it } from "vitest";
+installQuasarPlugin();
+
+describe("tests for AuthPage.vue", () => {
+  it("", () => {});
+});
