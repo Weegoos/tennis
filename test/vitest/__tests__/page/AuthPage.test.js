54071744aab52b2a0f16bf894e055e694a764096
Weegoos <batyr.ashim05@mail.ru>
2025-04-07 15:30:50 +0500

    Tennis: The AuthPage.test.js has been updated

--- test/vitest/__tests__/page/AuthPage.test.js
+++ test/vitest/__tests__/page/AuthPage.test.js
@@ -1,7 +1,37 @@
 import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
-import { describe, it } from "vitest";
+import { mount } from "@vue/test-utils";
+import { Quasar } from "quasar";
+import AuthPage from "src/pages/AuthPage.vue";
+import { describe, expect, it } from "vitest";
 installQuasarPlugin();
 
 describe("tests for AuthPage.vue", () => {
-  it("", () => {});
+  const wrapper = mount(AuthPage, {
+    global: {
+      plugins: [Quasar],
+    },
+  });
+  it("should find authorizationContent data-testid", () => {
+    const authorizationContent = wrapper.find(
+      '[data-testid="authorizationContent"]'
+    );
+    expect(authorizationContent.exists()).toBe(true);
+    expect(authorizationContent.text()).toContain("Authorization");
+    expect(authorizationContent).toMatchSnapshot();
+  });
+  it("should find emailInput data-testid", () => {
+    const emailInput = wrapper.find('[data-testid="emailInput"]');
+    expect(emailInput.exists()).toBe(true);
+
+    emailInput.setValue("Example@gmail.com");
+    expect(emailInput.element.value).toBe("Example@gmail.com");
+  });
+
+  it("should find passwordInput data-testid", () => {
+    const passwordInput = wrapper.find('[data-testid="passwordInput"]');
+    expect(passwordInput.exists()).toBe(true);
+
+    passwordInput.setValue("examplePassword");
+    expect(passwordInput.element.value).toBe("examplePassword");
+  });
 });
