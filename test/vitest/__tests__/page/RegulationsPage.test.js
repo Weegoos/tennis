dca50e984737e76df09bc2d169826219552dabd6
Weegoos <batyr.ashim05@mail.ru>
2025-04-08 16:02:33 +0500

    Tennis: The RegulationsPage.test.js has been created

---
+++ test/vitest/__tests__/page/RegulationsPage.test.js
@@ -0,0 +1,8 @@
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { describe, it } from "vitest";
+
+installQuasarPlugin();
+
+describe("tests for RegulationsPage.vue", () => {
+  it("", () => {});
+});
--- test/vitest/__tests__/page/TournamentsPage.test.js
+++ test/vitest/__tests__/page/TournamentsPage.test.js
@@ -1,8 +1,29 @@
 import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { flushPromises, mount } from "@vue/test-utils";
+import { Quasar } from "quasar";
+import TournamentPage from "src/components/IndexPage/TournamentPage.vue";
 import { describe, expect, it } from "vitest";
+import { nextTick } from "vue";
 
 installQuasarPlugin();
 
+let testData = {
+  data: {
+    description: "Desc",
+  },
+};
+
 describe("tests for TournamentsPage.vue", () => {
-  it("should find tournamentsContent data-testid", () => {});
+  const wrapper = mount(TournamentPage, {
+    global: {
+      plugins: [Quasar],
+    },
+  });
+  it("should find tournamentsID data-testid", async () => {
+    wrapper.vm.tournaments = testData;
+    await flushPromises();
+    await nextTick();
+    const tournamentsID = wrapper.find('[data-testid="tournamentsID"]');
+    // expect(tournamentsID.exists()).toBe(true);
+  });
 });
