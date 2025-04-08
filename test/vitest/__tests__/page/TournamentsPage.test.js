d84451f93745d28ad23c26492b89f47af76f3568
Weegoos <batyr.ashim05@mail.ru>
2025-04-08 15:52:47 +0500

    Tennis: The TournamentsPage.test.js has been created

---
+++ test/vitest/__tests__/page/TournamentsPage.test.js
@@ -0,0 +1,8 @@
+import { installQuasarPlugin } from "@quasar/quasar-app-extension-testing-unit-vitest";
+import { describe, expect, it } from "vitest";
+
+installQuasarPlugin();
+
+describe("tests for TournamentsPage.vue", () => {
+  it("should find tournamentsContent data-testid", () => {});
+});
