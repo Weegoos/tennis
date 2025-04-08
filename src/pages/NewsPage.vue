b910e1538150b15903c54ec8a60483b929524d4c
Weegoos <batyr.ashim05@mail.ru>
2025-04-08 16:24:46 +0500

    Tennis: The NewsPage.vue has been updated

--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -1,9 +1,51 @@
 <template>
-  <div>Новости</div>
+  <div class="q-pa-md">
+    <q-card class="my-card">
+      <q-img
+        src="https://cdn.quasar.dev/img/mountains.jpg"
+        :ratio="16 / 9"
+        spinner-color="primary"
+        spinner-size="82px"
+      />
+      <q-card-section>
+        <div class="text-h6">Our Changing Planet</div>
+        <div class="text-subtitle2">by John Doe</div>
+      </q-card-section>
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+import { useQuasar } from "quasar";
+import { getMethod } from "src/composables/apiMethod/get";
+import { getCurrentInstance, onMounted, ref } from "vue";
+
+// global variables
+const { proxy } = getCurrentInstance();
+const serverURL = proxy.$serverURL;
+const $q = useQuasar();
+
+const allNews = ref([]);
+const getAllNews = async () => {
+  try {
+    await getMethod(
+      serverURL,
+      "news/allNews?page=1&size=10",
+      allNews,
+      $q,
+      "Error: "
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  getAllNews();
+});
 </script>
 
 <style></style>
