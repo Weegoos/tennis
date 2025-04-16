cbcd729bae8e8f18747eabaa0fe0e221252a2d85
Weegoos <batyr.ashim05@mail.ru>
2025-04-16 16:10:18 +0500

    Tennis: The project has been updated

--- src/pages/Admin/AdminStats.vue
+++ src/pages/Admin/AdminStats.vue
@@ -6,34 +6,10 @@
       </q-card-section>
     </q-card>
   </q-page>
-  <q-page class="q-pa-sm">
-    <card-social icon_position="left" />
-
-    <card-charts />
-
-    <div class="row q-col-gutter-sm q-py-sm">
-      <tab-social />
-      <card-with-image />
-    </div>
-
-    <div class="row q-col-gutter-sm q-pb-sm">
-      <todo-list />
-
-      <card-time-line />
-    </div>
-
-    <table-visits />
-  </q-page>
 </template>
 
 <script setup>
-import {
-  getCurrentInstance,
-  onMounted,
-  ref,
-  watch,
-  defineAsyncComponent,
-} from "vue";
+import { getCurrentInstance, onMounted, ref, watch } from "vue";
 import * as echarts from "echarts";
 import { getMethod } from "src/composables/apiMethod/get";
 import { useQuasar } from "quasar";
@@ -105,77 +81,6 @@ watch([totalTournaments, totalUsers, totalPartner], () => {
 onMounted(() => {
   defineLength();
 });
-
-const mode = ref("list");
-const messages = ref([
-  {
-    id: 5,
-    name: "Pratik Patel",
-    msg:
-      " -- I'll be in your neighborhood doing errands this\n" +
-      "            weekend. Do you want to grab brunch?",
-    avatar: "https://avatars2.githubusercontent.com/u/34883558?s=400&v=4",
-    time: "10:42 PM",
-  },
-  {
-    id: 6,
-    name: "Winfield Stapforth",
-    msg:
-      " -- I'll be in your neighborhood doing errands this\n" +
-      "            weekend. Do you want to grab brunch?",
-    avatar: "https://cdn.quasar.dev/img/avatar6.jpg",
-    time: "11:17 AM",
-  },
-  {
-    id: 1,
-    name: "Boy",
-    msg:
-      " -- I'll be in your neighborhood doing errands this\n" +
-      "            weekend. Do you want to grab brunch?",
-    avatar: "https://cdn.quasar.dev/img/boy-avatar.png",
-    time: "5:17 AM",
-  },
-  {
-    id: 2,
-    name: "Jeff Galbraith",
-    msg:
-      " -- I'll be in your neighborhood doing errands this\n" +
-      "            weekend. Do you want to grab brunch?",
-    avatar: "https://cdn.quasar.dev/team/jeff_galbraith.jpg",
-    time: "5:17 AM",
-  },
-  {
-    id: 3,
-    name: "Razvan Stoenescu",
-    msg:
-      " -- I'll be in your neighborhood doing errands this\n" +
-      "            weekend. Do you want to grab brunch?",
-    avatar: "https://cdn.quasar.dev/team/razvan_stoenescu.jpeg",
-    time: "5:17 AM",
-  },
-]);
-
-const CardSocial = defineAsyncComponent(() =>
-  import("../../components/Admin/CardSocial.vue")
-);
-const CardCharts = defineAsyncComponent(() =>
-  import("../../components/Admin/CardCharts.vue")
-);
-const TabSocial = defineAsyncComponent(() =>
-  import("../../components/Admin/TabSocial.vue")
-);
-const CardWithImage = defineAsyncComponent(() =>
-  import("components/cards/CardWithImage.vue")
-);
-const CardTimeLine = defineAsyncComponent(() =>
-  import("components/cards/CardTimeLine.vue")
-);
-const TodoList = defineAsyncComponent(() =>
-  import("components/list/TodoList.vue")
-);
-const TableVisits = defineAsyncComponent(() =>
-  import("components/tables/TableVisits.vue")
-);
 </script>
 
 <style scoped>
