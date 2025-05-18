dad14392e79cde1a7a2b64efce349d576531a038
Weegoos <batyr.ashim05@mail.ru>
2025-05-18 10:54:12 +0500

    deploy

--- vuepress-starter/docs/.vuepress/.temp/internal/routes.js
+++ vuepress-starter/docs/.vuepress/.temp/internal/routes.js
@@ -5,20 +5,3 @@ export const routes = Object.fromEntries([
   ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"/home/batyr_ashim/Рабочий стол/Diploma/client/tennis/vuepress-starter/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Документация проекта \"Tennis\""} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/home/batyr_ashim/Рабочий стол/Diploma/client/tennis/vuepress-starter/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
