751c92cc589bc377631f04c4211a526347cb2ed1
Weegoos <batyr.ashim05@mail.ru>
2025-05-25 19:45:26 +0500

    Tennis: The walkover function has been updated

--- src/components/Tournaments/TournamentResult.vue
+++ src/components/Tournaments/TournamentResult.vue
@@ -1,32 +1,85 @@
 <template>
   <div>
-    <q-dialog v-model="isOpenWindowForResult" persistent>
-      <q-card>
+    <q-dialog
+      v-model="isOpenWindowForResult"
+      persistent
+      class="w-[450px] h-[450px]"
+    >
+      <q-card class="my-card">
         <q-card-section>
-          <q-select
-            v-model="winner"
-            :options="winnerOptions"
-            label="Winner"
-            option-label="label"
-            option-value="value"
-            emit-value
-            map-options
-          />
+          <div>
+            <q-splitter v-model="splitterModel" style="height: 250px">
+              <template v-slot:before>
+                <q-tabs v-model="tab" vertical class="text-teal">
+                  <q-tab name="result" icon="mdi-chart-bar" label="Result" />
+                  <q-tab name="walkover" icon="mdi-run-fast" label="WalkOver" />
+                </q-tabs>
+              </template>
 
-          <q-input
-            v-model="score"
-            type="text"
-            label="Score"
-            hint="For Example: 6:1 6:0"
-          />
+              <template v-slot:after>
+                <q-tab-panels
+                  v-model="tab"
+                  animated
+                  swipeable
+                  vertical
+                  transition-prev="jump-up"
+                  transition-next="jump-up"
+                >
+                  <q-tab-panel name="result">
+                    <q-card-section>
+                      <q-select
+                        v-model="winner"
+                        :options="winnerOptions"
+                        label="Winner"
+                        option-label="label"
+                        option-value="value"
+                        emit-value
+                        map-options
+                      />
+
+                      <q-input
+                        v-model="score"
+                        type="text"
+                        label="Score"
+                        hint="For Example: 6:1 6:0"
+                      />
+                    </q-card-section>
+                    <q-card-actions align="right" class="q-gutter-sm">
+                      <BaseCloseButton
+                        @click="closeTournamentResultWindow"
+                        label="Close"
+                      />
+                      <BasePostButton @click="sendInfo" label="Send" />
+                    </q-card-actions>
+                  </q-tab-panel>
+                  <q-tab-panel name="walkover">
+                    <q-card-section>
+                      <q-select
+                        v-model="winner"
+                        :options="winnerOptions"
+                        label="Winner"
+                        option-label="label"
+                        option-value="value"
+                        emit-value
+                        map-options
+                      />
+                    </q-card-section>
+                    <q-card-actions align="right" class="q-gutter-sm">
+                      <BaseCloseButton
+                        @click="closeTournamentResultWindow"
+                        label="Close"
+                      />
+                      <BasePostButton
+                        @click="sendWalkOverWinner"
+                        label="Send"
+                      />
+                    </q-card-actions>
+                  </q-tab-panel>
+                </q-tab-panels>
+              </template>
+            </q-splitter>
+          </div>
         </q-card-section>
-        <q-card-actions align="right" class="q-gutter-sm">
-          <BaseCloseButton
-            @click="closeDetailedInformationWindow"
-            label="Close"
-          />
-          <BasePostButton @click="sendInfo" label="Send" />
-        </q-card-actions>
       </q-card>
     </q-dialog>
   </div>
@@ -38,6 +91,7 @@ import BaseCloseButton from "../atoms/BaseCloseButton.vue";
 import BasePostButton from "../atoms/BasePostButton.vue";
 import { useQuasar } from "quasar";
 import { putMethod } from "src/composables/apiMethod/put";
+
 // global variables
 const props = defineProps({
   isOpenResultWindow: {
@@ -54,12 +108,13 @@ const props = defineProps({
   },
 });
 const { proxy } = getCurrentInstance();
-const serverURL = "http://localhost:8000/api/admin/";
+const adminURL = proxy.$adminURL;
 const $q = useQuasar();
 
 const winnerOptions = ref([]);
 const winner = ref("");
 const score = ref("");
+const tab = ref("result");
 
 const isOpenWindowForResult = ref(props.isOpenResultWindow);
 watch(
@@ -94,11 +149,30 @@ const sendInfo = async () => {
       score: score.value,
     };
     await putMethod(
-      "http://localhost:8000/api/admin/",
+      adminURL,
       `tournaments/matches/${props.matchID}/result`,
       payload,
       $q,
       "Good",
+      "Error: ",
+      {}
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const sendWalkOverWinner = async () => {
+  try {
+    const payload = {
+      winnerRegistrationId: winner.value,
+    };
+    await putMethod(
+      adminURL,
+      `tournaments/matches/${props.matchID}/walkover`,
+      payload,
+      $q,
+      "Walkover",
       "Error: ",
       {}
     );
