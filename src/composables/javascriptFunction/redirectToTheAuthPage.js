867f8d96b743ce80cddbc2eb3a6e8cc161116a5b
Weegoos <batyr.ashim05@mail.ru>
2025-05-25 20:49:20 +0500

    deploy

--- src/composables/javascriptFunction/redirectToTheAuthPage.js
+++ src/composables/javascriptFunction/redirectToTheAuthPage.js
@@ -1,5 +1,5 @@
 import { useApiStore } from "src/stores/api-store";
-const clientURL = "http://localhost:9000/#/";
+const clientURL = "https://kaz-tennis.netlify.app";
 async function redirectToUser() {
   try {
     const apiStore = useApiStore();
